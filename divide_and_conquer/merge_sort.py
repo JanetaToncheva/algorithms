86f5c239185854dd44496cdfa47f4864afd7d312
Janet T <janeta.toncheva@gmail.com>
2023-12-30 12:36:15 +0200

    merge sort

---
+++ divide_and_conquer/merge_sort.py
@@ -0,0 +1,36 @@
+def merge_sort(array):
+    if len(array) < 2:
+        return array
+    else:
+        mid = len(array)//2
+        left = array[:mid]
+        right = array[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right): # essentially since we divide by half each time this is only executed once
+            if left[i] <= right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+
+        return array
+
+
+print(merge_sort(array=[1, 5, 0]))
